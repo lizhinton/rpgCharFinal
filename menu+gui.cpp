2cf93bfc37133048a7a7989bae9bdbfffa8f1a30
lizhinton <127344028+lizhinton@users.noreply.github.com>
2024-03-28 12:24:48 -0500

    Add files via upload

---
+++ menu+gui.cpp
@@ -0,0 +1,24 @@
+//Liz Hinton
+//gui and menu control for the rpg character class project
+//3-28-24 -- worked on psuedocode framework from 10:39am - 10:46
+//made a very simple framework to be improved upon when I have access to others psuedocode and feel more confident in exactly what this program needs to do
+
+//menu control class
+	//variables
+	
+	//constructors
+	
+	//getters
+	
+	//setters
+	
+	//methods
+		//create character
+
+		//edit character
+
+		//save character
+
+		//view character
+
+		//gui (includes start, end, and runs the entire length of the program
